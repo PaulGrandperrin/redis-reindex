303e86658abecfbf5538c64be99fbafd6a89c2a1
Paul Grandperrin <paul.grandperrin@gmail.com>
2020-01-15 17:05:17 +0100

    use pipelined SETEX instead of SETM + many EXPIREAT

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,34 @@ use rayon::iter::ParallelBridge;
 use rayon::prelude::ParallelIterator;
 use itertools::Itertools;
 use std::collections::HashMap;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 const MSET_SIZE: usize = 100;
 
-fn injector(i: u32, counter: &AtomicU64, addr: &str, r: crossbeam_channel::Receiver<Vec<(Vec<u8>,Vec<u8>,Vec<u8>)>>) -> Result<(), failure::Error> {
+fn injector(i: u32, global_expired_count: &AtomicU64, global_inserted_count: &AtomicU64, addr: &str, r: crossbeam_channel::Receiver<Vec<(Vec<u8>,Vec<u8>,Vec<u8>)>>) -> Result<(), failure::Error> {
     let mut client = redis::Client::open(addr)?;
     let mut con = client.get_connection()?;
 
-    let mut count = 0;
+    let mut thread_inserted_count:u64 = 0;
+    let mut thread_expired_count: u64 = 0;
     let mut iteration = 0;
-    while let Ok(bulk) = r.recv() {
 
+    while let Ok(bulk) = r.recv() {
+        let mut loop_inserted_count = 0;
+        let mut loop_expired_count = 0;
+        let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as i64;
         let len = bulk.len();
 
         let mut pipe = redis::pipe();
-        let mut pipe = pipe.cmd("MSET");
-        for (key, value, _) in &bulk {
-            pipe = pipe.arg(key.to_owned()).arg(value.to_owned());
-        }
-        pipe = pipe.ignore();
-        for (key, _, expire_date) in bulk {
-            pipe = pipe.cmd("EXPIREAT").arg(key).arg(expire_date).ignore();
+        let mut pipe = &mut pipe;
+        for (key, value, expire_date) in bulk {
+            let ttl: i64 = String::from_utf8(expire_date).unwrap().parse::<i64>().unwrap() - now;
+            if ttl > 0 {
+                pipe = pipe.cmd("SET").arg(key).arg(value).arg("EX").arg(ttl.to_string()).ignore();
+                loop_inserted_count += 1;
+            } else {
+                loop_expired_count += 1;
+            }
         }
 
         loop {
@@ -54,9 +61,12 @@ fn injector(i: u32, counter: &AtomicU64, addr: &str, r: crossbeam_channel::Recei
             }
         }
 
-        count += len;
-        let cur = counter.fetch_add(len as u64, std::sync::atomic::Ordering::SeqCst);
-        println!("thread {:>4} injected {:>8} entries - total: {:>8}", i, count, cur + len as u64);
+        thread_inserted_count += loop_inserted_count;
+        thread_expired_count += loop_expired_count;
+
+        let gec = global_expired_count.fetch_add(loop_expired_count as u64, std::sync::atomic::Ordering::SeqCst);
+        let gic = global_inserted_count.fetch_add(loop_inserted_count as u64, std::sync::atomic::Ordering::SeqCst);
+        println!("thread {:>4}: inserted {:>8}, expired {:>8} | total: inserted {:>8}, expired {:>8}", i, thread_inserted_count, thread_expired_count, gic + loop_inserted_count as u64, gec + loop_expired_count as u64);
 
     }
 
@@ -67,18 +77,20 @@ fn main() -> Result<(), failure::Error> {
 
     let addr = std::env::args().nth(1).ok_or(failure::format_err!("missing redis addr"))?;
     let mut set_count = 0;
-    let counter = AtomicU64::new(0);
+    let global_inserted_count = AtomicU64::new(0);
+    let global_expired_count = AtomicU64::new(0);
     let mut hm = HashMap::new();
     crossbeam_utils::thread::scope(|scope| {
 
         let (s, r) = crossbeam_channel::bounded(1000  );
 
-        for i in 0..256 {
+        for i in 0..8 {
             let r = r.clone();
             let addr = addr.clone();
-            let a = &counter;
+            let gic = &global_inserted_count;
+            let gec = &global_expired_count;
             scope.spawn(move |_| {
-                injector(i, a, &addr, r).unwrap();
+                injector(i, gec, gic, &addr, r).unwrap();
             });
         }
 
@@ -121,7 +133,8 @@ fn main() -> Result<(), failure::Error> {
     });
 
     // execute SET commands without EXPIREAT
-    println!("There is {} keys remaining without an EXPIREAT", hm.len());
+    let without_ttl_count = hm.len() as u64;
+    println!("There is {} keys remaining without an EXPIREAT", without_ttl_count);
     let client = redis::Client::open(addr.as_str()).unwrap();
     let mut con = client.get_connection().unwrap();
     let mut pipe = redis::pipe();
@@ -132,8 +145,8 @@ fn main() -> Result<(), failure::Error> {
     pipe.query::<()>(&mut con).unwrap();
     println!("Done injecting keys without EXPIREAT");
 
-    println!("set count: {}", set_count);
-    println!("total injected: {}", counter.load(std::sync::atomic::Ordering::SeqCst));
+    println!("read SET count: {}", set_count);
+    println!("inserted {} + expired {} + without_ttl {} = {}", global_inserted_count.load(std::sync::atomic::Ordering::SeqCst), global_expired_count.load(std::sync::atomic::Ordering::SeqCst), without_ttl_count, global_inserted_count.load(std::sync::atomic::Ordering::SeqCst) + global_expired_count.load(std::sync::atomic::Ordering::SeqCst) + without_ttl_count);
 
     Ok(())
 }
