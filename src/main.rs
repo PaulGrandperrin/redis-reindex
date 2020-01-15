9bf671fc6335d58a0857c48f32d4475a246c7361
Paul Grandperrin <paul.grandperrin@gmail.com>
2020-01-15 13:45:30 +0100

    add expireat

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,30 @@ use std::sync::atomic::AtomicU64;
 use rayon::iter::ParallelBridge;
 use rayon::prelude::ParallelIterator;
 use itertools::Itertools;
+use std::collections::HashMap;
 
-const MSET_SIZE: usize = 200;
+const MSET_SIZE: usize = 400;
 
-fn injector(i: u32, counter: &AtomicU64, addr: &str, r: crossbeam_channel::Receiver<Vec<Vec<u8>>>) -> Result<(), failure::Error> {
+fn injector(i: u32, counter: &AtomicU64, addr: &str, r: crossbeam_channel::Receiver<Vec<(Vec<u8>,Vec<u8>,Vec<u8>)>>) -> Result<(), failure::Error> {
     let client = redis::Client::open(addr)?;
     let mut con = client.get_connection()?;
 
     let mut count = 0;
-    while let Ok(args) = r.recv() {
-        redis::cmd("MSET").arg(args).query(&mut con)?;
-        count += 1;
-        let cur = counter.fetch_add(MSET_SIZE as u64, std::sync::atomic::Ordering::SeqCst);
-        println!("{} -> {} * {} = {} - total: {}", i, count, MSET_SIZE, count * MSET_SIZE, cur + MSET_SIZE as u64);
+    while let Ok(bulk) = r.recv() {
+        let len = bulk.len();
+        let mut pipe = redis::pipe();
+        let mut pipe = pipe.cmd("MSET");
+        for (key, value, _) in bulk.iter() {
+            pipe = pipe.arg(key.to_owned()).arg(value.to_owned());
+        }
+        pipe = pipe.ignore();
+        for (key, _, expire_date) in bulk {
+            pipe = pipe.cmd("EXPIREAT").arg(key).arg(expire_date).ignore();
+        }
+        pipe.query(&mut con)?;
+        count += len;
+        let cur = counter.fetch_add(len as u64, std::sync::atomic::Ordering::SeqCst);
+        println!("thread {:>4} injected {:>8} entries - total: {:>8}", i, count, cur + len as u64);
     }
 
     Ok(())
@@ -30,52 +41,72 @@ fn injector(i: u32, counter: &AtomicU64, addr: &str, r: crossbeam_channel::Recei
 fn main() -> Result<(), failure::Error> {
 
     let addr = std::env::args().nth(1).ok_or(failure::format_err!("missing redis addr"))?;
-
+    let mut set_count = 0;
     let counter = AtomicU64::new(0);
+    let mut hm = HashMap::new();
     crossbeam_utils::thread::scope(|scope| {
 
-    let (s, r) = crossbeam_channel::bounded(1000);
+        let (s, r) = crossbeam_channel::bounded(1000  );
 
-    for i in 0..50 {
-        let r = r.clone();
-        let addr = addr.clone();
-        let a = &counter;
-        scope.spawn(move |_| {
-            injector(i, a, &addr, r).unwrap();
-        });
-    }
+        for i in 0..16 {
+            let r = r.clone();
+            let addr = addr.clone();
+            let a = &counter;
+            scope.spawn(move |_| {
+                injector(i, a, &addr, r).unwrap();
+            });
+        }
 
-    let stdin = std::io::stdin();
-    let reader = stdin.lock();
-    let mut parser = redis::Parser::new(reader);
+        let stdin = std::io::stdin();
+        let reader = stdin.lock();
+        let mut parser = redis::Parser::new(reader);
 
-    let mut mset_vec = Vec::with_capacity(MSET_SIZE * 2);
+        let mut mset_vec = Vec::with_capacity(MSET_SIZE);
 
-    while let Ok(redis::Value::Bulk(bulk)) = parser.parse_value() {
-        match bulk.as_slice() {
-            [redis::Value::Data(command), redis::Value::Data(key), redis::Value::Data(value)] if &*command == b"SET"  => {
-                if mset_vec.len() < mset_vec.capacity() {
-                    mset_vec.push(key.to_owned());
-                    mset_vec.push(value.to_owned());
-                } else {
-                    let mut tmp = Vec::with_capacity(MSET_SIZE * 2);
-                    std::mem::swap(&mut tmp, &mut mset_vec);
-                    s.send(tmp);
+
+
+
+        while let Ok(redis::Value::Bulk(bulk)) = parser.parse_value() {
+            match bulk.as_slice() {
+                [redis::Value::Data(command), redis::Value::Data(key), redis::Value::Data(value)] if &*command == b"SET"  => {
+                    assert_eq!(hm.insert(key.to_owned(), value.to_owned()), None);
+                    set_count += 1;
                 }
+                [redis::Value::Data(command), redis::Value::Data(key), redis::Value::Data(expire_date)] if &*command == b"EXPIREAT"  => {
+                    let (key, value) = hm.remove_entry(key).expect(&format!("Got EXPIREAT of unknown key: {}", String::from_utf8_lossy(key)));
 
-            }
-            [redis::Value::Data(command), redis::Value::Data(key), redis::Value::Data(date)] if &*command == b"EXPIREAT"  => {
-
-            }
-            [ref v @ ..] => {
-                //println!("Ignored command: {:?}", v)
+                    mset_vec.push((key, value, expire_date.to_owned()));
+                    if mset_vec.len() == mset_vec.capacity() {
+                        let mut tmp = Vec::with_capacity(MSET_SIZE);
+                        std::mem::swap(&mut tmp, &mut mset_vec);
+                        s.send(tmp);
+                    }
+                }
+                [ref c @ ..] => {
+                    println!("Ignored command: {:?}", c)
+                }
             }
         }
-    }
 
-    s.send(mset_vec);
+        s.send(mset_vec);
+
 
     });
+
+    // execute SET commands without EXPIREAT
+    println!("There is {} keys remaining without an EXPIREAT", hm.len());
+    let client = redis::Client::open(addr.as_str()).unwrap();
+    let mut con = client.get_connection().unwrap();
+    let mut pipe = redis::pipe();
+    let mut pipe = &mut pipe;
+    for (key, value) in hm {
+        pipe = pipe.cmd("SET").arg(key).arg(value).ignore();
+    }
+    pipe.query::<()>(&mut con).unwrap();
+    println!("Done injecting keys without EXPIREAT");
+
+    println!("set count: {}", set_count);
+    println!("total injected: {}", counter.load(std::sync::atomic::Ordering::SeqCst));
 
     Ok(())
 }
